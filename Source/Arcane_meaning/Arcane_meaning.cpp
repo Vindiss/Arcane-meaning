87277b46bd8d39ae768604fed0c79a18bc281cfb
Vindiss <147603163+Vindiss@users.noreply.github.com>
2026-09-16 14:59:19 +0200

    init projet

--- Source/Arcane_meaning/Arcane_meaning.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Arcane_meaning.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arcane_meaning, "Arcane_meaning" );
-
-DEFINE_LOG_CATEGORY(LogArcane_meaning)
--- Source/Arcane_meaning/Arcane_meaningGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Arcane_meaningGameMode.h"
-
-AArcane_meaningGameMode::AArcane_meaningGameMode()
-{
-	// stub
-}
